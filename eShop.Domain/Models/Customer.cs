8eb31374bd3772a2511df2886e5388748f0068ab
Andrei Serban <andreiserbanrazvan@gmail.com>
2020-11-19 15:32:47 +0200

    eShop Init Domain Model

---
+++ eShop.Domain/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Domain.Models
+{
+   public class Customer
+    {
+        public int Id { get; set; }
+        public Name Name { get; set; }
+        public long Ssn { get; set; }
+        public ICollection<ContactDetail> ContactDetails { get; set; }
+        public string Username { get; set; }
+    }
+}
